978501899ee6e104d41bbcf31f5f107dfa2e129c
marcoesposito_di <esposito@di.uniroma1.it>
2021-11-10 10:04:51 +0100

    add first compiled slides

--- compile_win_macos.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Author: Marco Esposito
-# Based on instructions by the MARP developing team.
-
-# Run this script on bash to compile the presentation in html format.
-# This script is only intended to be used on Windows or MacOS
-
-docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli $1 $2
