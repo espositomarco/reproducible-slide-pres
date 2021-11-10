64677a9934680b533191c780b08d13e3edf782c3
marcoesposito_di <esposito@di.uniroma1.it>
2021-11-10 10:03:12 +0100

    fix input bug in compile_linux.sh

--- compile_linux.sh
+++ compile_linux.sh
@@ -6,10 +6,13 @@
 # Run this script on bash to compile the presentation in html format.
 # This script is only intended to be used on Linux.
 
+if [ -z ${2} ]; then echo "compiling to HTML"; else FLAG=--allow-local-files; echo "compiling to PDF"; fi
+
+
 docker run --rm \
 	-v $PWD:/home/marp/app/ \
 	-e MARP_USER="$(id -u):$(id -g)" \
 	-e LANG=$LANG marpteam/marp-cli \
-	--allow-local-files \
+	${FLAG} \
 	$1 \
 	$2
